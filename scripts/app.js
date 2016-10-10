cc3fd938c3f6ce682ff52c160f1ddef0b319aa48
chitownhope <apr_dec@hotmail.com>
2016-10-10 00:46:43 -0500

    linked some more modals...styling and moving them is still a problem...and then some more tweaking in other areas.

--- scripts/app.js
+++ scripts/app.js
@@ -2,12 +2,41 @@
  
  
  
- // pop up for trunk below
-    $('#trunk').hover(function() {
+ // pop up for each clue element below
+$('#trunk').hover(function() {
     $('#trunk-pop-up').show();
    }, function() {
      $('#trunk-pop-up').hide();
    });
  
- 
+$('#telescope').hover(function() {
+    $('#telescope-pop-up').show();
+   }, function() {
+     $('#telescope-pop-up').hide();
+   });
+
+$('#brown-bear').hover(function() {
+    $('#brown-bear-pop-up').show();
+   }, function() {
+     $('#brown-bear-pop-up').hide();
+   });
+  
+$('#red-chair').hover(function() {
+	$('#red-chair-pop-up').show();
+	}, function() {
+ 	 $('#red-chair-pop-up').hide();
+	});
+  
+ $('#green-phone').hover(function() {
+	$('#green-phone-pop-up').show();
+	}, function() {
+ 	 $('#green-phone-pop-up').hide();
+	});
+
+$('#big-trophy').hover(function() {
+	$('#big-trophy-pop-up').show();
+	}, function() {
+ 	 $('#big-trophy-pop-up').hide();
+	});
+
  // });
